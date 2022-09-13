942c9596f0940d33e50a8149405339f94c701d33
Aan Bahudin <aan.bahudin11@gmail.com>
2022-09-13 21:16:03 +0800

    footer design fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <section className="mt-auto py-10 px-20 bg-purple-400 w-full">
+        <section className="mt-[10%] py-10 px-20 bg-sky-800 text-white w-full">
             <h1 className="text-center text-sm font-kanit">Copyright 2022 Aan Bahudin</h1>
         </section>
     )
