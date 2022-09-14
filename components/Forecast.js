1ba2a6bb0797516bfe693a84934924fbe07ad3f9
Aan Bahudin <aan.bahudin11@gmail.com>
2022-09-14 22:25:42 +0800

    forecast responsive

--- components/Forecast.js
+++ components/Forecast.js
@@ -12,7 +12,7 @@ const Forecast = () => {
     return (
         <section className="mt-20 w-[80%] mx-auto grid auto-cols-auto auto-rows-auto gap-8 text-[1.1rem]">
 
-            <div data-aos="fade-up" className="row-start-1 row-end-3 col-start-1 col-end-6 grid grid-cols-2 rounded backdrop-blur-3xl bg-white/[20%] dark:bg-cardDark duration-200">
+            <div data-aos="fade-up" className="row-start-1 row-end-3 col-start-1 col-end-5 md:col-end-6 grid grid-cols-1 sm:grid-cols-2 rounded backdrop-blur-3xl bg-white/[20%] dark:bg-cardDark duration-200">
 
                 <section className="flex items-center p-4 font-kanit justify-center flex-col">
                     <h1 className="text-3xl text-sky-800 dark:text-silver">{moment(currentForecast.date).format('LL')}</h1>
@@ -20,7 +20,7 @@ const Forecast = () => {
                     <h1 className="text-3xl text-white dark:text-darkSecondary">{currentForecast.day.condition.text}</h1>
                 </section>
 
-                <section className="flex justify-center font-kanit my-auto items-center gap-3 p-4 flex-wrap">
+                <section className="hidden sm:flex justify-center font-kanit my-auto items-center gap-3 p-4 flex-wrap">
                     {Object.keys(forecastInfo.location).slice(0,6).map((item, index) => {
                         return ( 
                             <div key={index} className="flex flex-wrap my-auto justify-center space-x-2">
@@ -33,7 +33,7 @@ const Forecast = () => {
                 </section>
             </div>
 
-            <div className="md:row-start-3 md:row-end-6 md:col-start-1 md:col-end-4 py-4 px-2 flex gap-2 flex-wrap items-center justify-evenly rounded backdrop-blur-3xl bg-white/[20%] dark:bg-cardDark duration-200">
+            <div className="row-start-3 row-end-6 col-start-1 col-end-5 md:row-start-3 md:row-end-6 md:col-start-1 md:col-end-4 py-4 px-2 flex gap-2 flex-wrap items-center justify-evenly rounded backdrop-blur-3xl bg-white/[20%] dark:bg-cardDark duration-200">
                     {filtering(currentForecast.day, filterData).map((item, index) => {
                         return (
                             <h5 key={index} className="flex flex-row gap-x-2 text-left">
@@ -45,7 +45,7 @@ const Forecast = () => {
                     })}
             </div>
 
-            <div className="md:row-start-3 md:row-end-6 p-4 md:col-start-4 md:col-end-6 flex gap-2 flex-wrap items-center justify-evenly rounded backdrop-blur-3xl bg-white/[20%] dark:bg-cardDark duration-200">
+            <div className="row-start-6 row-end-9 p-4 col-start-1 col-end-5 md:row-start-3 md:row-end-6 md:col-start-4 md:col-end-6 flex gap-2 flex-wrap items-center justify-evenly rounded backdrop-blur-3xl bg-white/[20%] dark:bg-cardDark duration-200">
                     {Object.keys(currentForecast.astro).slice(0,5).map((key, index) => {
                         const newkey = (key.split('_'));
                         return (
