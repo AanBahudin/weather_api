bdaac1e1ad2e2db807de5591f0d5c9e6566f6fc7
Aan Bahudin <aan.bahudin11@gmail.com>
2022-09-14 20:01:52 +0800

    search & autocomplete responsive

--- components/Autocomplete.js
+++ components/Autocomplete.js
@@ -7,7 +7,7 @@ const AutoComplete = () => {
     const {allWeather, getSingelWeather, handleSearch, loading} = useWeatherContext()
 
     return (
-        <section data-aos="fade" className="w-[40%] z-10 bg-white/30 backdrop-blur-3xl dark:bg-cardDark duration-200 border-[1px] border-white/50 mx-auto overflow-y-auto h-[30vh] px-5 rounded mt-1 absolute left-[28%]">
+        <section data-aos="fade" className="w-[90%] z-10 bg-white/30 backdrop-blur-3xl dark:bg-cardDark duration-200 border-[1px] border-white/50 mx-auto overflow-y-auto h-[30vh] px-5 rounded mt-1 absolute top-[100%]">
             <section className={`flex flex-col justify-around`}>
             {loading ? <Loading /> : (
                 allWeather.map((item) => {
@@ -16,7 +16,7 @@ const AutoComplete = () => {
                             <section className="cursor-auto font-kanit text-left hover:bg-sky-400 p-2 rounded dark:hover:bg-silver duration-200 dark:fill-darkSecondary text-sky-800 dark:text-darkSecondary">
                                 <span className='flex gap-x-2'>
                                     <ImLocation2 size={20} className='fill-white/80 dark:fill-darkSecondary my-auto' />
-                                    <h1 className="max-w-[80%] duration-200 text-[1.1rem]"> {item.name}, {item.region}, {item.country}</h1>
+                                    <h1 className="max-w-full xl:max-w-[80%] duration-200 text-[1rem] xl:text-[1.1rem]"> {item.name}, {item.region}, {item.country}</h1>
                                 </span>
                             </section>
                         </div>
--- components/Search.js
+++ components/Search.js
@@ -9,16 +9,15 @@ const Search = () => {
     const {searchValue, handleSearch, handleTheme, theme} = useWeatherContext()
 
     return (
-        <section className="w-full text-center relative text-white dark:text-silver"> 
-            <h1 className="font-kanit duration-200 my-[2%]">Forecast WebApp</h1>
+        <section className="w-full text-center relative mt-[30%] md:mt-0 text-white dark:text-silver"> 
+            <h1 className="font-kanit duration-200 my-2">Forecast WebApp</h1>
 
-            <div className='flex mx-auto justify-center'>
-                <input value={searchValue} onChange={e => handleSearch(e.target.value)} type='text' placeholder="Search ..." className="text-sans font-dosis text-center w-[40%] rounded-lg bg-transparent dark:placeholder:text-silver placeholder:text-white border-[1px] border-white/80  py-2 px-4 ring-0 outline-none" />
-                <button onClick={() => handleSearch('')} className='my-auto ml-1 py-2 px-4 border-[1px] active:bg-sky-800 dark:active:bg-silver bg-lightPrimary active:text-black duration-150 border-white dark:border-silver rounded-lg'> <AiOutlineClose className='fill-white' /> </button>
+            <div className='flex mx-auto justify-center w-[90%]'>
+                <input value={searchValue} onChange={e => handleSearch(e.target.value)} type='text' placeholder="Search ..." className="text-sans font-dosis text-center w-full rounded bg-transparent dark:placeholder:text-silver placeholder:text-white border-[1px] border-white/80  py-2 px-4 ring-0 outline-none" />
+                {/* <button onClick={() => handleSearch('')} className='my-auto ml-1 py-2 px-4 border-[1px] active:bg-sky-800 dark:active:bg-silver bg-lightPrimary active:text-black duration-150 border-white dark:border-silver rounded-lg'> <AiOutlineClose className='fill-white' /> </button> */}
+                {searchValue.length >= 3 ? <AutoComplete /> : null}
             </div>
-            {searchValue.length >= 3 ? <AutoComplete /> : null}
         <button className='duration-200 z-50 fixed top-4 fill-white right-8' onClick={() => {handleTheme(!theme)}}>{theme ? <BsFillSunFill /> : <BsFillMoonFill />}</button>
-        <button></button>
         </section>
     )
 }
